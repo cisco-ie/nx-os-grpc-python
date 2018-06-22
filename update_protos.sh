753318aef085adb4f44da80f0ed390f3710d85c2
Remington Campbell <remcampb@cisco.com>
2018-06-22 15:36:32 -0700

    Update pathing

--- update_protos.sh
+++ update_protos.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python -m grpc_tools.protoc --proto_path=. --python_out=nx-os-grpc-python/proto --grpc_python_out=nx-os-grpc-python/proto nxos_grpc.proto
+python -m grpc_tools.protoc --proto_path=. --python_out=nxos_grpc/proto --grpc_python_out=nxos_grpc/proto nxos_grpc.proto
