11447a2e969d29a6095c661b2b2a5a5d95271aa1
Remington Campbell <remcampb@cisco.com>
2018-06-22 18:09:18 -0700

    Relocate and recompile proto

--- update_protos.sh
+++ update_protos.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python -m grpc_tools.protoc --proto_path=. --python_out=nxos_grpc/proto --grpc_python_out=nxos_grpc/proto nxos_grpc.proto
+python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. nxos_grpc/proto/nxos_grpc.proto
